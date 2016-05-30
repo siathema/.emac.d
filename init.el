1130f6ba271483243b27b4082764eb0151747065
Matthias <nate.holaday@gmail.com>
2016-05-29 22:39:25 -0700

    mac osx support

--- init.el
+++ init.el
@@ -16,3 +16,28 @@
 ;; Learn about it on http://www.emacswiki.org/emacs/ELPA
 (when (not package-archive-contents)
   (package-refresh-contents))
+
+(defvar my-packages
+  '())
+
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+  (add-to-list 'my-packages 'exec-path-from-shell))
+
+;; Iterate over the list of packages and install
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Setup exec-path-from-shell package for Mac
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs
+    '("PATH")))
